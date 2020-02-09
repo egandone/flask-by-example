da5db34d62816692e5c2c404172afdcff71cefde
Egan Done <edone@sympatico.ca>
2020-02-09 12:24:34 -0500

    Pretty much complete part-1 with working WSGI via nginx

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+
+@app.route('/')
+def hello():
+    return "flask-by-example"
+
+@app.route('/<name>')
+def hello_name(name):
+    return f'Hello {name}!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
