4925ead4f28a147e0a812205df8f078055aa7f4f
Egan Done <edone@sympatico.ca>
2020-02-09 14:43:42 -0500

    Added in database stuff (part 2)

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
 
 
 class ProductionConfig(Config):
